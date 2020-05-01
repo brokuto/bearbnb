788e545be8b5e2e0dd2672ba5b57dd44d6364fe1
taaurce1 <celina.urwank@swisscom.com>
2020-05-01 17:35:40 +0200

    implemented rent date and rent modal

--- src/app/components/objects/objects.component.ts
+++ src/app/components/objects/objects.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-objects',
@@ -10,24 +11,51 @@ import { Observable } from 'rxjs';
 export class ObjectsComponent implements OnInit {
 
   items: Observable<any[]>;
+  rentForm: FormGroup;
 
   modalId;
   openModal;
+  openRentModal;
+
+  rentDate: Date;
+  fromDate: Date;
+  toDate: Date;
 
   constructor(
-    firestore: AngularFirestore
+    firestore: AngularFirestore,
+    private fb: FormBuilder
   ) {
     this.items = firestore.collection('items').valueChanges();
+    this.createForm();
   }
 
   ngOnInit() {
     this.openModal = false;
+    this.openRentModal = false;
+  }
+
+  createForm() {
+    this.rentForm = this.fb.group({
+      fromDate: ['', Validators.required],
+      toDate: ['', Validators.required]
+    });
   }
 
 
   getId(id: number) {
     this.openModal = true;
     this.modalId = id;
+  }
+
+  rentModal(id: number) {
+    this.openRentModal = true;
+  }
+
+  rent(fromDate: Date, toDate: Date) {
+    this.fromDate = fromDate;
+    this.toDate = toDate;
+
+    this.openRentModal = false;
   }
 
 
